22913bb5b586de65301eb5e4c24e3ea5698cac51
Fergago20 <fergomez@uamv.edu.ni>
2024-09-17 19:37:16 -0600

    Ejercicio

---
+++ ReservacionAerolinea/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ReservacionAerolinea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblBoardingPass = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.rdoNonSmoking = new System.Windows.Forms.RadioButton();
+            this.rdoSmoking = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(272, 240);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 66);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 240);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblBoardingPass
+            // 
+            this.lblBoardingPass.AutoSize = true;
+            this.lblBoardingPass.Location = new System.Drawing.Point(233, 324);
+            this.lblBoardingPass.Name = "lblBoardingPass";
+            this.lblBoardingPass.Size = new System.Drawing.Size(0, 20);
+            this.lblBoardingPass.TabIndex = 11;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnSubmit.Location = new System.Drawing.Point(32, 346);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(160, 53);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Asignar Asiento";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click_1);
+            // 
+            // rdoNonSmoking
+            // 
+            this.rdoNonSmoking.AutoSize = true;
+            this.rdoNonSmoking.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoNonSmoking.Location = new System.Drawing.Point(132, 253);
+            this.rdoNonSmoking.Name = "rdoNonSmoking";
+            this.rdoNonSmoking.Size = new System.Drawing.Size(123, 29);
+            this.rdoNonSmoking.TabIndex = 9;
+            this.rdoNonSmoking.TabStop = true;
+            this.rdoNonSmoking.Text = "No Fumar";
+            this.rdoNonSmoking.UseVisualStyleBackColor = true;
+            // 
+            // rdoSmoking
+            // 
+            this.rdoSmoking.AutoSize = true;
+            this.rdoSmoking.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoSmoking.Location = new System.Drawing.Point(388, 253);
+            this.rdoSmoking.Name = "rdoSmoking";
+            this.rdoSmoking.Size = new System.Drawing.Size(98, 29);
+            this.rdoSmoking.TabIndex = 8;
+            this.rdoSmoking.TabStop = true;
+            this.rdoSmoking.Text = "Fumar";
+            this.rdoSmoking.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label1.Location = new System.Drawing.Point(27, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Por favor selecciona tu preferencia de asiento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label2.Location = new System.Drawing.Point(165, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 32);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "PROGRAMA DE AEROLINEA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label3.Location = new System.Drawing.Point(56, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 29);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Agregar un pasajero";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombre.Location = new System.Drawing.Point(272, 110);
+            this.tbNombre.Multiline = true;
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(229, 46);
+            this.tbNombre.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 25);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Nombre del pasajero:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblBoardingPass);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rdoNonSmoking);
+            this.Controls.Add(this.rdoSmoking);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblBoardingPass;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RadioButton rdoNonSmoking;
+        private System.Windows.Forms.RadioButton rdoSmoking;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ReservacionAerolinea/Form1.cs
@@ -0,0 +1,53 @@
+﻿using ReservacionAerolinea.clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReservacionAerolinea
+{
+    public partial class Form1 : Form
+    {
+        private NumAsientos avion;
+
+        public Form1()
+        {
+            InitializeComponent();
+            avion = new NumAsientos();
+        }
+
+        private void btnSubmit_Click_1(object sender, EventArgs e)
+        {
+            int section = rdoSmoking.Checked ? 1 : 2;
+            int seleccionarNum = avion.AsignarAsiento(section);
+            string nombre = null;
+
+            if (seleccionarNum == -1)
+            {
+                string OtraSeccion = (section == 1) ? "No Fumar" : "Fumar";
+                if (MessageBox.Show($"La sección está llena. ¿Te gustaría ser asignado a la sección de {OtraSeccion}?",
+                    "Sección llena", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    section = (section == 1) ? 2 : 1;
+                    seleccionarNum = avion.AsignarAsiento(section);
+                }
+            }
+
+            if (seleccionarNum != -1)
+            {
+                nombre = tbNombre.Text;
+                string NombreSeccion = (section == 1) ? "Fumar" : "No Fumar";
+                lblBoardingPass.Text = $"Pasajero: {nombre}\nAsiento asignado: {seleccionarNum}\nSección: {NombreSeccion}";
+            }
+            else
+            {
+                lblBoardingPass.Text = "El próximo vuelo sale en 3 horas.";
+            }
+        }
+    }
+}
